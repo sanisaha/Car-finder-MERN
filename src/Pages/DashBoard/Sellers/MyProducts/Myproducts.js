28600c9212b6c04b62df4f9355917d676d0faa9d
sanisaha <44751151+sanisaha@users.noreply.github.com>
2022-11-27 19:07:44 +0200

    myproducts load when not booked

--- src/Pages/DashBoard/Sellers/MyProducts/Myproducts.js
+++ src/Pages/DashBoard/Sellers/MyProducts/Myproducts.js
@@ -1,27 +1,16 @@
-import React, { useContext } from 'react';
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
-import { toast } from 'react-toastify';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../../../Context/AuthProvider';
 
 
 const MyProducts = () => {
-
     const { user } = useContext(AuthContext);
+    const [availableProducts, setAvailAbleProducts] = useState([]);
+    useEffect(() => {
+        fetch(`http://localhost:5000/cars/${user?.email}`)
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, [user])
 
-    const handleProductAdd = (event) => {
-        event.preventDefault();
-        const form = event.target;
-        const name = form.name.value;
-        const photoURL = form.photoURL.value;
-        const product = {
-            name: name,
-            picture: photoURL,
-            date: new Date()
-        }
-        console.log(product);
-
-    }
     // fetch('https://carry-you-server.vercel.app/reviews', {
     //         method: 'POST',
     //         headers: {
@@ -38,24 +27,6 @@ const MyProducts = () => {
     //         })
     //         .catch(e => console.error(e))
 
-    return (
-        <form onSubmit={handleProductAdd} className='text-center'>
-            <div className='py-5'>
-                <h1 className='text-3xl font-semibold'>Add Your Product Here</h1>
-            </div>
-            <div>
-                <input type='text' name='message' className="input input-borderd w-1/2" placeholder="write your product name"></input>
-            </div>
-            <div>
-                <input type='text' name='photoURL' className="input input-borderd w-1/2" placeholder="product's valid photoURL"></input>
-            </div>
-
-            <div className='py-5'>
-                <input className='btn btn-submit' type="submit" value="Add Product" />
-            </div>
-            <ToastContainer />
-
-        </form>
-    );
+    return
 };
 export default MyProducts;
